74e0865f1ce59349a632c7955663af583be0250f
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-01-22 17:45:07 -0300

    feat: adicionando dynamodb a aplicação

--- src/main/java/com/leodelmiro/cliente/dataprovider/repository/ClienteRepository.java
+++ src/main/java/com/leodelmiro/cliente/dataprovider/repository/ClienteRepository.java
@@ -1,13 +1,12 @@
 package com.leodelmiro.cliente.dataprovider.repository;
 
-import com.leodelmiro.cliente.dataprovider.repository.entity.CPFEntity;
 import com.leodelmiro.cliente.dataprovider.repository.entity.ClienteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-@Repository
-public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
-    Optional<ClienteEntity> findClienteByCpf(CPFEntity cpf);
+@EnableScan
+public interface ClienteRepository extends CrudRepository<ClienteEntity, String> {
+    Optional<ClienteEntity> findClienteByCpf(String cpf);
 }
--- src/main/java/com/leodelmiro/cliente/dataprovider/repository/mapper/ClienteEntityMapper.java
+++ src/main/java/com/leodelmiro/cliente/dataprovider/repository/mapper/ClienteEntityMapper.java
@@ -1,5 +1,6 @@
 package com.leodelmiro.cliente.dataprovider.repository.mapper;
 
+import com.leodelmiro.cliente.core.domain.CPF;
 import com.leodelmiro.cliente.dataprovider.repository.entity.ClienteEntity;
 import com.leodelmiro.cliente.core.domain.Cliente;
 import org.mapstruct.Mapper;
@@ -7,6 +8,14 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ClienteEntityMapper {
     ClienteEntity toClienteEntity(Cliente cliente);
+
+    default String map(CPF cpf) {
+        return cpf != null ? cpf.getCpf() : null;
+    }
+
+    default CPF map(String cpf) {
+        return cpf != null ? new CPF(cpf) : null;
+    }
 
     Cliente toCliente(ClienteEntity clienteEntity);
 }
